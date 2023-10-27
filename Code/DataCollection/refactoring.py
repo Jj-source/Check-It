db358858438223c1cb5da6da24801862e1aa1b91
TommasoCaselli <tcaselli@RN-145-90-178-92.eduroam.rug.nl>
2023-10-27 13:31:13 +0200

    revised version of the repo

---
+++ Code/DataCollection/refactoring.py
@@ -0,0 +1,75 @@
+import codice.articleObj as articleObj
+import json
+
+# code to clean and refactor the dataset
+
+FILEPAGELLA = ''
+
+with open(FILEPAGELLA, "r") as json_file:
+    f = json.load(json_file)
+    DATASET = "../outputs/dataset.txt"
+    #clean the file from previous writes
+    open(DATASET, 'w').close()
+
+    dataset = []
+    for dict in f:
+        for art in dict:
+            title = art["title"]["rendered"]
+            title = articleObj.bonifica(title)
+            verdettoVerboso = ""
+            counter = 0
+            try:
+                data = art["acf"]["data_della_dichiarazione"]
+                data = articleObj.bonifica(data)
+            except:
+                data = ""
+            try:
+                statement = art["acf"]["sentenza"]
+                statement = articleObj.bonifica(statement)
+            except:
+                statement = ""
+            try:
+                source = art["acf"]["link"]
+                source = articleObj.bonifica(source)
+            except:
+                source = ""
+            try:
+                verdetto = art["acf"]["verdetto"]["testo"]
+                verdetto = articleObj.bonifica(verdetto)
+            except:
+                verdetto = ""
+            try:
+                counter = len(art["acf"]["verdetto"]["in_breve"])
+                for str in art["acf"]["verdetto"]["in_breve"]:
+                    if(counter == 1):
+                        verdettoVerboso += "\t\t\"" + articleObj.bonifica(str["testo"]) + "\""
+                    else:
+                        verdettoVerboso += "\t\t\"" + articleObj.bonifica(str["testo"]) + "\"," + "\n"
+                    counter -= 1
+            except:
+                verdettoVerboso = ""
+            try:
+                content = art["content"]["rendered"]
+                content = art.bonifica(content)
+                #dividiamolo in singole frasi
+                newContent = content.split('.')
+            except:
+                newContent = ""
+            try:
+                del art["editor"]
+            except:
+                pass
+            try:
+                del art["_links"]
+            except:
+                pass
+
+            #links elaboration
+
+
+
+            p1 = articleObj.articleObj(title,data,statement,verdetto,verdettoVerboso,source,newContent)
+            dataset.append(p1)
+            p1.writeToFile(DATASET)
+
+###########
---
+++ Paraphrases/exp_camoscio/run_camoscio.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_0.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_50.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_100.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_150.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_200.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_250.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_300.json
+python parafrasi_camoscio.py /projects/tcaselli/Documents/clic-it2023-camoscio/camoscio/data_split/test_split_350.json
+
+
+
